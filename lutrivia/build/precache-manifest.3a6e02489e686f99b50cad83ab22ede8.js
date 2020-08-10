990479a9612defe38f4e64e28c7e42293a9ec136
iona-b <ionabrabender@gmail.com>
2020-08-10 12:53:54 -0700

    Done.

---
+++ lutrivia/build/precache-manifest.3a6e02489e686f99b50cad83ab22ede8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c497514bfddf60be187fb21a0efb34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "139623ca24622b7c13a9",
+    "url": "/static/js/main.4a262a22.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ lutrivia/build/static/js/main.4a262a22.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(n,e,t){n.exports=t(9)},9:function(n,e,t){"use strict";t.r(e);var r=t(0),o=t.n(r),a=t(2);var c=function(){return o.a.createElement("div",null)};t.n(a).a.render(o.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.4a262a22.chunk.js.map
